1772e2b786b49b54d440b72a04d53334d221b5ad
wuyongwen <wuyongwen@gmail.com>
2016-06-22 14:41:34 +0800

    add

--- weixin-java-common/src/main/java/me/chanjar/weixin/common/bean/result/WxErrorMsgUtil.java
+++ weixin-java-common/src/main/java/me/chanjar/weixin/common/bean/result/WxErrorMsgUtil.java
@@ -1,17 +1,15 @@
 package me.chanjar.weixin.common.bean.result;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.testng.collections.Maps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -19,9 +17,11 @@ import com.google.gson.reflect.TypeToken;
 import me.chanjar.weixin.common.util.StringUtils;
 
 public class WxErrorMsgUtil {
+	private static Logger log = LoggerFactory.getLogger(WxErrorMsgUtil.class);
 	private static final Map<Integer, String> errorMsg = new HashMap<>();
 	private static final Gson gson = new Gson();
 	static {
+		log.info("加载error.json");
 		initMap();
 	}
 
@@ -46,8 +46,11 @@ public class WxErrorMsgUtil {
 		BufferedReader reader = null;
 		String laststr = "";
 		try {
-			InputStream is=WxErrorMsgUtil.class.getClass().getResourceAsStream("/me/chanjar/weixin/common/bean/result/error.json");   
-	        BufferedReader br=new BufferedReader(new InputStreamReader(is));  
+			//Thread.currentThread().getContextClassLoader().getClass().getResourceAsStream("/me/chanjar/weixin/common/bean/result/error.json");   
+			InputStream is = WxErrorMsgUtil.class.getResourceAsStream("error.json");
+			if(is==null) return "";
+	        log.info("读取error.json文件");
+			BufferedReader br=new BufferedReader(new InputStreamReader(is));  
 			reader = new BufferedReader(br);
 			String tempString = null;
 			while ((tempString = reader.readLine()) != null) {
@@ -55,7 +58,7 @@ public class WxErrorMsgUtil {
 			}
 			reader.close();
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.error("读取文件错误!",e);
 		} finally {
 			if (reader != null) {
 				try {
